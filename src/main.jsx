435ce257aa7ffff31d6ef431fb5b91dbe60fbdff
Laboratorio Computación N° 6 <claseslab6@inacap.cl>
2025-06-10 15:51:34 -0400

    login, card y .env

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppRouter from './router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<AppRouter />);
