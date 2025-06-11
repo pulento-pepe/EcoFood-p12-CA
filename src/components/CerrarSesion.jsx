20d98b918f2b2f21b945a825529d156044fae8b7
Laboratorio Computación N° 1 <claseslab1@inacap.cl>
2025-06-11 13:45:22 -0400

    correcciones

--- src/components/CerrarSesion.jsx
+++ src/components/CerrarSesion.jsx
@@ -1,9 +1,23 @@
-function Home() {
+import { signOut } from "firebase/auth";
+import { auth } from "../services/firebase";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+
+export default function CerrarSesion() {
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+        try {
+            await signOut(auth);
+            Swal.fire("Sesión cerrada", "Has cerrado sesión correctamente", "success");
+            navigate("/login");
+        // eslint-disable-next-line no-unused-vars
+        } catch (error) {
+            Swal.fire("Error", "No se pudo cerrar la sesión", "error");
+        }
+    };
     return (
-    <div>
-    <h2>Bienvenido a EcoFood</h2>
-    <CerrarSesion />
-    </div>
+        <button onClick={handleLogout} className="btn btn-danger">
+        Cerrar Sesión
+        </button>
     );
-    }
-    
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,24 @@
+import { useEffect } from "react";
+
 import CardProducto from '../components/CardProductos';
 import { getUserData } from "../services/userService";
 import { useAuth } from "../context/AuthContext";
+import CerrarSesion from "../components/CerrarSesion";
 
-useEffect(() => {
-    const fetch = async () => {
-    const datos = await getUserData(user.uid);
-    setUserData(datos);
-    };
-    if (user) fetch();
-    }, [user])
 
 function Home() {
+    
+   /* useEffect(() => {
+        const fetch = async () => {
+        const datos = await getUserData(user.uid);
+        setUserData(datos);
+        };
+        if (user) fetch();
+    }, [user])*/
     return (
         <div className="container mt-4">
+            CerrarSesion
+            <CerrarSesion />
             <h1>Productos Disponibles</h1>
             <CardProducto nombre="Pan Integral" precio="$500" />
             <CardProducto nombre="Pan Integral" precio="$500" />
